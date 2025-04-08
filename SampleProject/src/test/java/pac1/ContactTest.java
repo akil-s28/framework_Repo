52689ee4008363fede857a940fd91e8e7bcad7e5
akil-s28 <solkarakil97@gmail.com>
2025-04-08 16:43:08 +0530

    2nd commit

---
+++ SampleProject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,16 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void CreateCotactTest() {
+		System.out.println("execute CreateCotactTest");
+	}
+	
+		@Test
+		public void ModifyContactTest() {
+			System.out.println("execute ModifyContactTest");
+		}
+
+}
---
+++ SampleProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void CreateOrgTest() {
+		System.out.println("execute CreateOrgTest");
+	}
+	
+		@Test
+		public void ModifyOrgTest() {
+			System.out.println("execute ModifyOrgTest");
+		}
+
+
+}
